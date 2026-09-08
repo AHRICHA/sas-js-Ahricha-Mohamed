ebf60a92ca85b8e48b42601ddb0c9b564e52779b
AHRICHA MOHAMED <medhricha5@gmail.com>
2026-09-08 14:34:04 +0100

    guide-01

--- day01/exercices/exercice-01.js
+++ day01/exercices/exercice-01.js
@@ -17,7 +17,6 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-
 const PI = 3.14;
 const rayon = 5;
 const périmètre = (2 * PI * rayon );
--- day01/guides/guide-01.js
+++ day01/guides/guide-01.js
@@ -9,4 +9,9 @@
  */
 'use strict';
 
-// TODO: écris ta solution ici.
+let a = 5;
+let b = 10;
+
+[a, b] = [b, a];
+
+console.log(a, b);

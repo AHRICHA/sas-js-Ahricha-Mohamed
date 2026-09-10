849096799d41cc15574e812bbdbf0fe511da8c73
AHRICHA MOHAMED <medhricha5@gmail.com>
2026-09-10 14:32:23 +0100

    ex2&3

--- day04/exercices/exercice-02.js
+++ day04/exercices/exercice-02.js
@@ -14,4 +14,6 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+const produit = (a, b) => a * b
+
+console.log(produit(8, 9));

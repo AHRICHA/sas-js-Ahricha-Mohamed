00b7eec3604c8d91068bef42315e0551167a859d
AHRICHA MOHAMED <medhricha5@gmail.com>
2026-09-11 17:34:25 +0100

    day05

--- day05/exercices/exercice-10.js
+++ day05/exercices/exercice-10.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function domaine(email1) {
+       return email1.slice(email1.indexOf("@") + 1); //email.slice(index)
+}
+let x=domaine("ekekek@ananj")
+console.log(x);

0d4dbae56139687073664a0e9ae01e67fdafa640
AHRICHA MOHAMED <medhricha5@gmail.com>
2026-09-11 12:27:01 +0100

    ex4,5

--- day05/exercices/exercice-04.js
+++ day05/exercices/exercice-04.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+function verificationemail(email) {
+    return email.includes("@") 
+}
+console.log(verificationemail("jhjh@jjj"));

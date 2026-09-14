db8574f7d7111e687429627251913c0782a71413
AHRICHA MOHAMED <medhricha5@gmail.com>
2026-09-14 11:37:34 +0100

    guides , le 7 premiere exercicesDay06

---
+++ day06/guides/guide-02.js
@@ -0,0 +1,18 @@
+/**
+ * JOUR 06 — GUIDE 02
+ * Parcourir avec for...of
+ *
+ * OBJECTIF
+ * Vous avez notes = [12, 15, 8, 19]. Calculez la moyenne des notes en parcourant le tableau.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let notes = [12, 15, 18, 19]
+let somme = 0
+for (let i=0; i<notes.length; i++){
+    somme = somme+notes[i]
+}
+let moyenne = somme/4
+console.log(moyenne);
---
+++ day06/guides/guide-03.js
@@ -0,0 +1,15 @@
+/**
+ * JOUR 06 — GUIDE 03
+ * Manipulation (Ajouter et Supprimer)
+ *
+ * OBJECTIF
+ * Vous avez une file d'attente file = ["Alice", "Bob", "Charlie"]. "Alice" est servie (elle sort de la file par l'avant), et "David" rejoint la file (à l'arrière).
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+const file = ["Alice", "Bob", "Charlie"]
+file.splice(0, 1, "David")
+console.log(file);
+
